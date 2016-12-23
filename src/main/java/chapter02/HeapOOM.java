9538cd4b65c4b7b56f1ac80fe7de0622db95137d
charlie <1151832099@qq.com>
2016-12-23 18:07:07 +0800

    a

---
+++ src/main/java/chapter02/HeapOOM.java
@@ -0,0 +1,24 @@
+package chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args：jvm参数
+ * -verbose:gc 输出虚拟机中gc的详细情况 
+ * -Xms20m 最小堆内存 -Xmx20m 最大堆内存
+ * -XX:+HeapDumpOnOutOfMemoryError JVM会在遇到OutOfMemoryError时拍摄一个“堆转储快照”，并将其保存在一个文件中
+ */
+public class HeapOOM {
+
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+}
