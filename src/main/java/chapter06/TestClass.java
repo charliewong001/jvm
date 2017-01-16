95ae853c024b950bca6a6c89f282261d6075afd9
charlie <1151832099@qq.com>
2017-01-16 11:38:49 +0800

    a

---
+++ src/main/java/chapter06/TestClass.java
@@ -0,0 +1,14 @@
+package chapter06;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        String name = "Louis";
+        greeting(name);
+    }
+
+    public static void greeting(String name) {
+        System.out.println("Hello," + name);
+    }
+
+}
