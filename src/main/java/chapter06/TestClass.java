7c0440810650f324064c43ccb29b0810e515b81c
charlie <1151832099@qq.com>
2017-07-04 15:21:58 +0800

    A

--- src/main/java/chapter06/TestClass.java
+++ src/main/java/chapter06/TestClass.java
@@ -3,13 +3,17 @@ package chapter06;
 public class TestClass {
 
     public static void main(String[] args) {
-        //        String name = "Louis";
-        //        greeting(name);
+        String name = "Louis";
+        greeting(name);
         int[][][] array = new int[1][0][-1];
     }
 
     public static void greeting(String name) {
         System.out.println("Hello," + name);
+    }
+
+    class Inner {
+
     }
 
 }
